910228eee2fe255161fec91cb8e840849aa46b35
etuncay <etuncay@subu.edu.tr>
2021-05-05 15:06:52 +0300

    svelte project

---
+++ src/App.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { Router } from 'svelte-router-spa'
+    import { routes } from './routes/routes'
+    let options = { gaPageviews: true}
+</script>
+<Router {routes}   {options} />
